065e0de439cf0e3919fa7252f3f2a8d4b2cf9afe
Michael Couper <yanisilver6@yahoo.com>
2025-08-24 12:27:24 -0700

    Montserrat

--- resources/views/react/head.blade.php
+++ resources/views/react/head.blade.php
@@ -50,6 +50,11 @@ sort($jsFiles);
 sort($cssFiles);
 ?>
 
+<!-- Google Fonts - Montserrat -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
 {{-- Static assets that don't change between builds --}}
 <link rel="icon" href="/react/favicon.ico" />
 <link rel="apple-touch-icon" href="/react/logo180.png" />
